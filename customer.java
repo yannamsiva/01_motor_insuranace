c6edd00f3f6fea63bca5070b4f2238ef6771d522
yannamsiva <yannamsiva@gmail.com>
2026-03-23 14:10:21 +0530

    mono mehtod update

--- customer.java
+++ customer.java
@@ -4,6 +4,10 @@ class Customer
 int cno=100;
     String cname="shiva";
 double price=250.25;
+void mono()
+{
+	System.out.println("its ok);
+}
 void displayDetails()
 {
 System.out.println("Modificatin completed");
